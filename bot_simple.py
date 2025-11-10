24fd90d7325e2c61f4110ff26932fd192626ddbd
Randa LAKAB <lakabranda@gmail.com>
2025-11-10 07:18:33 +0100

    Update bot_simple.py

--- bot_simple.py
+++ bot_simple.py
@@ -2,7 +2,7 @@ from telegram.ext import Application, MessageHandler, filters
 
 TELEGRAM_TOKEN = "********"  # Your API Key here
 
-#Predefined 
+#Predefined answers 
 RESPONSES = {
     "hello": "👋 Hello! I'm Uni-Pal, your hackathon assistant!",
     "hi": "👋 Hi! How can I help you with the hackathon?",
